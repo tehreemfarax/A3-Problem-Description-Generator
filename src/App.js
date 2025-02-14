cf81776a237ec3835299717535343c257683c47f
Sohaib Alam <sohaib.alam@sator-it.com>
2025-02-14 23:05:20 +0500

    Resolve merge conflicts

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
-import ContactUs from './components/ContactUs';
 import Footer from './components/Footer';
 
 function App() {
